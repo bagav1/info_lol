84956cdefab10934aa46ac4c5569a40111def3be
bagav1 <bragaru@hotmail.es>
2022-05-01 20:19:46 -0500

    Initial

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,29 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const version_lol = 'https://ddragon.leagueoflegends.com/api/versions.json';
+const actual_version = version_lol[0];
+
 export const environment = {
-  production: false
+  production: false,
+
+  version_lol,
+  actual_version,
+  rotation_champ:'https://la1.api.riotgames.com/lol/platform/v3/champion-rotations',
+  queue:'https://static.developer.riotgames.com/docs/lol/queues.json',
+  url_all_champs: 'http://ddragon.leagueoflegends.com/cdn/' + actual_version + '/data/es_MX/champion.json',
+  url_champ: 'http://ddragon.leagueoflegends.com/cdn/' + actual_version + '/data/es_MX/champion/',
+  url_items:'http://ddragon.leagueoflegends.com/cdn/' + actual_version + '/data/es_MX/item.json',
+  url_summoner_spell:'http://ddragon.leagueoflegends.com/cdn/' + actual_version + '/data/es_MX/summoner.json',
+  url_profile_icon:'http://ddragon.leagueoflegends.com/cdn/' + actual_version + '/data/es_MX/profileicon.json',
+  img_splash:'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/',
+  img_loading:'http://ddragon.leagueoflegends.com/cdn/img/champion/loading/',
+  img_champ:'http://ddragon.leagueoflegends.com/cdn/' + actual_version + '/img/champion/',
+  img_passive:'http://ddragon.leagueoflegends.com/cdn/' + actual_version + '/img/passive/',
+  img_spell:'http://ddragon.leagueoflegends.com/cdn/' + actual_version + '/img/spell/',
+  img_item:'http://ddragon.leagueoflegends.com/cdn/' + actual_version + '/img/item/',
+  img_summoner_spell:'http://ddragon.leagueoflegends.com/cdn/' + actual_version + '/img/spell/',
+  img_profile_icon:'http://ddragon.leagueoflegends.com/cdn/' + actual_version + '/img/profileicon/',
 };
 
 /*
